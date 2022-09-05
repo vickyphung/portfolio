05d811b9230a90676dd3d4b7d84fa7bd44fb4fcd
vicky phung <vickyphung@gmail.com>
2022-09-05 13:09:37 -0400

    updated contacts and projects

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,44 +1,54 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
-import './style.css'
+import "./style.css";
 
-export default function Contact () {
+export default function Contact() {
+  const socialLinks = [
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg",
+      url: "https://github.com/vickyphung",
+    },
 
-// const socialLinks = [
-//   {img:https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg},
-//   {img:https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png},
-//   {img:https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Circle-icons-mail.svg/2048px-Circle-icons-mail.svg.png},
-// ]
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png",
+      url: "https://www.linkedin.com/in/vicky-phung/",
+    },
 
-
-
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Circle-icons-mail.svg/2048px-Circle-icons-mail.svg.png",
+      url: "mailto:vicky@vicky.wtf",
+      email:"vicky@vicky.wtf"
+    },
+  ];
 
   return (
-    <div className='container'>
+    <div className="container">
+      <div className="content">
+        <div className="contactIcons">
+          <h2>
+            <span className="contactTitle">Contact</span>
+          </h2>
 
-      <div className='content'>
-        <div className='contactIcons'>
-        <h2><span className='contactTitle'>Contact</span></h2>
-
-{/* 
-        {socialLinks.map(() => (
-          <img src=`{socialLink.img}`></img>
-        )} */}
-
-
-        <motion.div
-          className="box"
-          whileHover={{ scale: 1.25 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          >
-            <div className='contactInfo'><a href="https://github.com/vickyphung"><img className="socialIcon" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github"></img></a></div>
-        </motion.div>
-          <div className='contactInfo'><a href="https://www.linkedin.com/in/vicky-phung/"><img className="socialIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="linkedin"></img></a></div>
-        
-          <div className='contactInfo'><a href="mailto:vicky@vicky.wtf"><img className="socialIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Circle-icons-mail.svg/2048px-Circle-icons-mail.svg.png" alt="email"></img></a> <p>vicky@vicky.wtf</p></div>
-        
+          {socialLinks.map((socialLink) => (
+            <motion.div
+              className="box"
+              whileHover={{ scale: 1.25 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <div className="contactInfo">
+                <a href={socialLink.url}>
+                  <img
+                    className="socialIcon"
+                    src={socialLink.img}
+                    alt="github"
+                  ></img>
+                </a>
+                {socialLink.email}
+              </div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,142 +1,112 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 import { motion } from "framer-motion";
-import { ArrowUpIcon } from '@chakra-ui/icons';
+import { ArrowUpIcon } from "@chakra-ui/icons";
 
+export default function Projects() {
+const projects = [
+{
+    title: "📍 Places",
+    description: "Places to go in the DMV",
+    github:
+    "https://github.com/vickyphung/places-frontend , https://github.com/vickyphung/places_backend",
+    demo: "xxxx",
+    tech: `Users can view, search, post, favorite, and review places to go. Search and add places using Google Maps Places API. Full-CRUD MERN app.`,
+    id: 1,
+    },
+    {
+    title: "🪐 APOD",
+    description: "Astronomy Picture of the Day",
+    github: "https://github.com/vickyphung/APOD",
+    demo: "http://apodwithbear.herokuapp.com/",
+    tech: `View daily APOD, a random number of posts, or pick by date. React SPA, NASA APOD API.`,
+    id: 2,
+    },
+    {
+    title: "🂡 Blackjack",
+    description: "Blackjack Card Game",
+    github: "https://github.com/vickyphung/blackjack",
+    demo: "http://blackjackwithbear.netlify.com/",
+    tech: "Players have a $1,000 bankroll. Vanilla JS.",
 
-export default function Projects () {
-
-    const projects = [
-        {
-        title: '📍 Places',
-        description: 'Places to go in the DMV',
-        github: 'https://github.com/vickyphung/places-frontend , https://github.com/vickyphung/places_backend',
-        demo: 'xxxx',
-        tech: `Users can view, search, post, favorite, and review places to go. Search and add places using Google Maps Places API. Full-CRUD MERN app.`,
-        id: 1,
-        },
-        {
-        title: '🪐 APOD',
-        description: 'Astronomy Picture of the Day',
-        github: 'https://github.com/vickyphung/APOD',
-        demo: 'http://apodwithbear.herokuapp.com/',
-        tech: `View daily APOD, a random number of posts, or pick by date. React SPA, NASA APOD API.`,
-        id: 2,
-        },
-        {
-        title: '🂡 Blackjack',
-        description: 'Blackjack Card Game',
-        github: 'https://github.com/vickyphung/blackjack',
-        demo: 'http://blackjackwithbear.netlify.com/',
-        tech: 'Players have a $1,000 bankroll. Vanilla JS.',
-
-        id: 3,
-        },
-        {
-        title: '🌞 Weather',
-        description: 'Current, Hourly, Weekly Weather',
-        github: 'https://github.com/vickyphung/weather-react',
-        demo: 'http://weatherwithbear.herokuapp.com/',
-        tech: 'View weather by zipcode search, provides geolocation info., dark mode option. React SPA, openweather API.',
-        id: 4,
-        },
-        {
-        title: '🂡 Baccarat',
-        description: 'Baccarat Card Game',
-        github: 'https://github.com/vickyphung/baccarat',
-        demo: 'http://baccaratwithbear.netlify.com/',
-        tech: 'Players have a $10,000 bankroll. Still in development. Vanilla JS.',
-        id: 5,
-        },
-        {
-        title: '👾 USS Pinkybear',
-        description: 'Space Battle Console Game',
-        github: 'https://github.com/vickyphung/spacebattle',
-        demo: 'http://usspinkybear.netlify.com/',
-        tech: 'OOP, vanilla JS.',
-        id: 6,
-        },
-    ];
+    id: 3,
+    },
+    {
+    title: "🌞 Weather",
+    description: "Current, Hourly, Weekly Weather",
+    github: "https://github.com/vickyphung/weather-react",
+    demo: "http://weatherwithbear.herokuapp.com/",
+    tech: "View weather by zipcode search, provides geolocation info., dark mode option. React SPA, openweather API.",
+    id: 4,
+    },
+    {
+    title: "🂡 Baccarat",
+    description: "Baccarat Card Game",
+    github: "https://github.com/vickyphung/baccarat",
+    demo: "http://baccaratwithbear.netlify.com/",
+    tech: "Players have a $10,000 bankroll. Still in development. Vanilla JS.",
+    id: 5,
+    },
+    {
+    title: "👾 USS Pinkybear",
+    description: "Space Battle Console Game",
+    github: "https://github.com/vickyphung/spacebattle",
+    demo: "http://usspinkybear.netlify.com/",
+    tech: "OOP, vanilla JS.",
+    id: 6,
+},
+];
 
 const container = {
-    // hidden: { opacity: 1, scale: 0 },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-        delayChildren: 0.25,
-        staggerChildren: 0.35
-        }
-    }
-    };
+visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+    delayChildren: 0.25,
+    staggerChildren: 0.35,
+    },
+},
+};
 
-    const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1
-    }
-    };
-    return (
-        <div className='container'>
-
-
-                
-
-  
-
-<motion.ul
+const item = {
+hidden: { y: 20, opacity: 0 },
+visible: {
+    y: 0,
+    opacity: 1,
+},
+};
+return (
+<div className="container">
+    <motion.ul
     className="projectList"
     variants={container}
     initial="hidden"
     animate="visible"
->   
+    >
     {projects.map((project) => (
+        <motion.li
+        variants={item}
+        project={project}
+        key={project.id}
+        className="projectListItem"
+        >
+        <h2>
+            <span className="projectTitle">{project.title}</span>
+        </h2>
+
         <motion.li
             variants={item}
             project={project}
-            key={project.id} 
-            className="projectListItem" 
+            key={project.id}
+            className="projectItem"
         >
-        <h2>
-            <span className='projectTitle'>
-                {project.title}
-            </span>
-        </h2>
-
-
-        {/* <div>{project.description}</div>
-        <div>{project.demo}</div>
-        <div>{project.tech}</div> */}
-
-
-
-            <motion.li
-                variants={item}
-                project={project}
-                key={project.id} 
-                className="projectItem" 
-            >
-                <div>{project.description}</div>
-                <div>{project.demo}</div>
-                <div>{project.tech}</div>
-            </motion.li>
-
-
-
-
-        
-    </motion.li>
-
-))}        
-</motion.ul>
-        </div>
-    )
+            <div>{project.description}</div>
+            <p>{project.demo}</p>
+            <div>{project.tech}</div>
+        </motion.li>
+        </motion.li>
+    ))}
+    </motion.ul>
+</div>
+);
 }
-
-
-
-
-
-
-
