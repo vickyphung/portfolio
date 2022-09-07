e6fb0926b7ea036b89b86bfc46ef99c74f8d0eff
vicky phung <vickyphung@gmail.com>
2022-09-07 15:27:12 -0400

    removed menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,7 +20,7 @@ export default function Navigation() {
           </Link>
         </div>
 
-        <Menu bg="white">
+        {/* <Menu bg="white">
           <MenuButton
             as={IconButton}
             icon={<HamburgerIcon />}
@@ -39,7 +39,7 @@ export default function Navigation() {
               </CustomLink>
             </MenuItem>
           </MenuList>
-        </Menu>
+        </Menu> */}
       </div>
     </nav>
   );
