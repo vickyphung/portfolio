78433195ff5672fa7068ea77696e6be059e7a48c
vicky phung <vickyphung@gmail.com>
2022-09-07 15:32:04 -0400

    removed background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home';
-import Background from './components/Background';
+// import Background from './components/Background';
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/spaceground" element={<Background />} />
+          {/* <Route path="/spaceground" element={<Background />} /> */}
  
         </Routes>
     
