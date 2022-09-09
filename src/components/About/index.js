6c0c7ab9b5c531228d6ba0cd4969e2b2011eae6d
vicky phung <vickyphung@gmail.com>
2022-09-09 00:23:49 -0400

    updated motion for skill icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import './style.css'
-
+import React from "react";
+import "./style.css";
+import { motion } from "framer-motion";
 
 import expressImage from "../../images/expressLogo.png";
 import mongoImage from "../../images/mongodb.svg";
@@ -10,38 +10,76 @@ import htmlImage from "../../images/html5.png";
 import reactImage from "../../images/react.png";
 import cssImage from "../../images/css3.png";
 import jsImage from "../../images/JSlogo.png";
-import exImage from "../../images/expressjs-icon.svg"
-
-
-
-
+import exImage from "../../images/expressjs-icon.svg";
 
 function About() {
+  const container = {
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.25,
+        staggerChildren: 0.35,
+      },
+    },
+  };
 
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <div className='about'>
+    <div className="about">
       <div>
-      <h2><span className='intro'>Hi, I'm a Software Engineer. Hire me!</span></h2>
+        <h2>
+          <span className="intro">Hi, I'm a Software Engineer. Hire me!</span>
+        </h2>
       </div>
-    
-      <div className='skillsIcons'>
-      <img src={htmlImage} className="skillSetIcon" alt="Logo" />
-      <img src={cssImage} className="skillSetIcon" alt="Logo" />
-      <img src={jsImage} className="skillSetIcon" alt="Logo" />
-      <img src={mongoImage} className="skillSetIcon" alt="Logo" />
-      <img src={exImage} className="skillsetIcon" alt="Logo" />
-      <img src={reactImage} className="skillSetIcon" alt="Logo" />
-      <img src={nodeImage} className="skillSetIcon" alt="Logo" />
+
+      <div className="skillsIcons">
+        <motion.ul
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="skillsIcons"
+        >
+          <motion.li variants={item}>
+            <img src={htmlImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={cssImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={jsImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={mongoImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={exImage} className="skillsetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={reactImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+          <motion.li variants={item}>
+            <img src={nodeImage} className="skillSetIcon" alt="Logo" />
+          </motion.li>
+        </motion.ul>
       </div>
 
       <div>
-      <p><span className='skillSet'>
-        NoSQL, SQL, APIs;
-        Technical Writing, Communication, Research, Conflict Resolution
-      </span></p>
+        <p>
+          <span className="skillSet">
+            NoSQL, SQL, APIs; Technical Writing, Communication, Research,
+            Conflict Resolution
+          </span>
+        </p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default About
+export default About;
